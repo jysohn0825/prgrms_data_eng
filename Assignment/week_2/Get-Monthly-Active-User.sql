32008ffaca44f9dfb70884b17034581d934cced3
RB_ <jysohn0825@naver.com>
2020-08-26 20:27:34 +0900

    Update Get-Monthly-Active-User.sql

--- Assignment/week_2/Get-Monthly-Active-User.sql
+++ Assignment/week_2/Get-Monthly-Active-User.sql
@@ -2,8 +2,8 @@
 -- 매달 Active 한 사용자 선별하는 쿼리
 *********************************************************/
 SELECT
-  
-  /* LEFT(ts, 7) or DATE_TRUNC(‘month’, ts) 와 같은 의미이고 DATE_TRUNC는 timestamp, to_char는 string 리턴 */
+ 
+  -- LEFT(ts, 7) or DATE_TRUNC(‘month’, ts) 와 같은 의미이고 DATE_TRUNC는 timestamp, to_char는 string 리턴
   to_char(st.ts, 'YYYY-MM') as Monthly, 
   count(DISTINCT usc.userid) as ActiveUser
 FROM
